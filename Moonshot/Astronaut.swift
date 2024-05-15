48b706e0196092c180a5a23608d23216d46df75c
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-14 18:51:50 -1000

    added JSON decoding and date formatter

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Arseniy Oksenoyt on 5/14/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
