4a24b1c0957adddf36ffc8d270624a9b631aeaf4
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-14 14:19:08 -1000

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Arseniy Oksenoyt on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
